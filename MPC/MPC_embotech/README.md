857665d64551f2069aa6ffb4e89366b39461b78a
subdiduntil2 <manosserlis@gmail.com>
2023-02-24 19:08:15 +0200

    forces pro implementation added

---
+++ MPC/MPC_embotech/README.md
@@ -0,0 +1,27 @@
+# MPCC
+This is a Python software implementation of the MPC controller with a custom hybrid (kinematic-dynamic) model, using parameters of the P22 car.
+
+### Solver
+This version uses the FORCESPro solver 6.0.1 version. Users with license are connected to the forces.embotech.com server and results are being sent back to them.
+
+### Input rate cost and constraints - Dynamics
+To be written
+
+### Tire constraints
+To be written
+
+### Cost Function
+To be written
+
+### License installation
+To be analytically written. For license holders, it is also included on the website of FORCESPro, at the "What information do i need to submit?" section.
+
+### MPC Execution
+
+```
+pip3 install -r P23-DV/MPC/MPC_embotech/requirements.txt
+sudo apt-get install gcc libomp-dev
+export PYTHONPATH="path/to/embotech":$PYTHONPATH
+python3 P23-DV/MPC/MPC_embotech/mpc_prom.py
+```
+
---
+++ MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/DependInfo.cmake
@@ -0,0 +1,44 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Constraints/bounds.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Constraints/bounds.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Constraints/constraints.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Constraints/constraints.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Cost/cost.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Cost/cost.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Interfaces/hpipm_interface.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Interfaces/hpipm_interface.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Interfaces/solver_interface.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Interfaces/solver_interface.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/MPC/mpc.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/MPC/mpc.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Model/integrator.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Model/integrator.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Model/model.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Model/model.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Params/params.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Params/params.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Params/track.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Params/track.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Plotting/plotting.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Plotting/plotting.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Spline/arc_length_spline.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Spline/arc_length_spline.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Spline/cubic_spline.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Spline/cubic_spline.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Tests/constratins_test.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Tests/constratins_test.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Tests/cost_test.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Tests/cost_test.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/Tests/spline_test.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/Tests/spline_test.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/main.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/main.cpp.o"
+  "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/types.cpp" "/home/subdiduntil/P23-DV/MPC/MPC_unofficial/C++/CMakeFiles/MPCC.dir/types.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "External/blasfeo/lib/include"
+  "External/hpipm/lib/include"
+  "External/matplotlib"
+  "External/Eigen"
+  "External/Json/include"
+  "/usr/include/python3.8"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
