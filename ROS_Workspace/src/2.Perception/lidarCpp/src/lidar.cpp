3591eee711e813d8ffd8a9b7021bff9c6304358c
Paris Stentoumis <paris.s.1998@gmail.com>
2023-04-11 23:12:41 +0300

    added log and changes at timestamp

--- ROS_Workspace/src/2.Perception/lidarCpp/src/lidar.cpp
+++ ROS_Workspace/src/2.Perception/lidarCpp/src/lidar.cpp
@@ -33,15 +33,18 @@ LidarNode::LidarNode(): Node("lidar_node") {
     };
     using std::placeholders::_1;
     point_cloud_sub_ = create_subscription<sensor_msgs::msg::PointCloud2>(
-        "/hesai/pandar", 10, std::bind(&LidarNode::segment_point_cloud, this, _1)
+        "/hesai/pandar", sensor_qos, std::bind(&LidarNode::segment_point_cloud, this, _1)
     );
+
+    RCLCPP_INFO(this->get_logger(), "Initialization Lidar Inference node finished");
+
+    
 
 }
 
 LidarNode::~LidarNode() {
     segments.clear();
     prototype_points.clear();
-    myfile.close();
 }
 
 void LidarNode::declare_params() {
@@ -50,12 +53,15 @@ void LidarNode::declare_params() {
 }
 
 void LidarNode::segment_point_cloud(const sensor_msgs::msg::PointCloud2::SharedPtr msg) {
+    RCLCPP_INFO(this->get_logger(), "sub");
     myfile << "--,--,FRAME"<<frame_index<<",--,--,--\n";
+    RCLCPP_INFO(this->get_logger(), "wrote-");
 
     sensor_msgs::PointCloud2Iterator<float> xyzIter(*msg, "x");
     sensor_msgs::PointCloud2Iterator<uint16_t> ringIter(*msg, "ring");
     sensor_msgs::PointCloud2Iterator<float> iIter(*msg, "intensity");
     sensor_msgs::PointCloud2Iterator<double> timeIter(*msg, "timestamp");
+    RCLCPP_INFO(this->get_logger(), "initialized iterators");
 
     // for (int i=0; i<msg->fields.size(); i++) {
     //     std::cout << msg->fields[i].name << std::endl;
@@ -63,9 +69,11 @@ void LidarNode::segment_point_cloud(const sensor_msgs::msg::PointCloud2::SharedP
 
     for (; xyzIter != xyzIter.end(); ++xyzIter, ++ringIter, ++iIter, ++timeIter) {
 
-        myfile << xyzIter[0] << "," << xyzIter[1] << "," << xyzIter[2]  << "," << ringIter[0] << "," << iIter[0] << "," << timeIter[0] << std::endl;
+        myfile << xyzIter[0] << "," << xyzIter[1] << "," << xyzIter[2]  << "," << ringIter[0] << "," << iIter[0] << "," << msg->header.stamp.sec << "." << msg->header.stamp.nanosec << "," << timeIter[0] << std::endl;
     }
-
+    RCLCPP_INFO(this->get_logger(), "finished cloud");
+    
+    std::cout << frame_index << std::endl;
     frame_index++;
 }
 }
