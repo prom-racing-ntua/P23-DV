5557b56b6402e243a2885c66daa489115d313e4a
dpapailiopoulos <d.papailiopoulos.promracing@gmail.com>
2023-04-11 23:13:30 +0300

    lidar driver corrections

--- ROS_Workspace/src/1.Drivers/hesai_lidar/launch/hesai_lidar_launch.py
+++ ROS_Workspace/src/1.Drivers/hesai_lidar/launch/hesai_lidar_launch.py
@@ -10,8 +10,10 @@ def generate_launch_description():
             executable ='hesai_lidar_node',
             name ='hesai_node',
             output ="screen",
+            emulate_tty=True,
             parameters=[
-                {"pcap_file": "./src/1.Drivers/hesai_lidar/config/test_lidar.pcap"},
+                # PCAP file path needs to be the relative path from the root of the hesai_lidar package (i.e. "pcapFiles/test_lidar.pcap")
+                {"pcap_file": ""},
                 {"server_ip"  : "192.168.1.201"},
                 {"lidar_recv_port"  : 2368},
                 {"gps_port"  : 10110},
@@ -19,13 +21,13 @@ def generate_launch_description():
                 {"lidar_type"  : "PandarXT-32"},
                 {"frame_id"  : "PandarXT-32"},
                 {"pcldata_type"  : 0},
-                {"publish_type"  : "both"},
+                {"publish_type"  : "points"},
                 {"timestamp_type"  : ""},
                 {"data_type"  : ""},
                 {"lidar_correction_file"  : "./src/1.Drivers/hesai_lidar/config/PandarXT-32.csv"},
                 {"multicast_ip"  : ""},
                 {"coordinate_correction_flag"  : False},
-                {"fixed_frame"  : "lidar"},
+                {"fixed_frame"  : 'lidar'},
                 {"target_frame_frame"  : ""}
             ]
         )
