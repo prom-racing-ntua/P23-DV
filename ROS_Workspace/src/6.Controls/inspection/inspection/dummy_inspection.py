4435fd9ca665223c1a6b6c624ac4a799f7454633
dpapailiopoulos <d.papailiopoulos.promracing@gmail.com>
2023-07-04 05:45:14 +0300

    small inspection change

--- ROS_Workspace/src/6.Controls/inspection/inspection/dummy_inspection.py
+++ ROS_Workspace/src/6.Controls/inspection/inspection/dummy_inspection.py
@@ -24,8 +24,9 @@ class InspectionMission(Node):
         self._prev_send = 0.0
         self._reached_goal = False
 
-        # self._steering_command = self.declare_parameter('steering', 0.0).value
-        self._steering_command = MAX_STEERING
+        self._steering_command = self.declare_parameter('steering', 0.0).value
+        self._brake_command = self.declare_parameter('brake', 0.0).value
+        # self._steering_command = MAX_STEERING
 
         self._command_publisher = self.create_publisher(TxControlCommand ,'/control_commands', 10)
         self._state_publisher = self.create_publisher(TxSystemState ,'/system_state', 10)
@@ -42,21 +43,21 @@ class InspectionMission(Node):
     def set_steering(self, msg:RxSteeringAngle) -> None:
         self._steering_angle = msg.steering_angle
         # self.get_logger().info(f"Received rack displacement {self._steering_angle}")
-        if abs(self._steering_angle - self._steering_command) < 1e-3 and not self._reached_goal and self._send_time is not None:
-            self._reached_goal = True
-            response_time = self.get_time() - self._send_time
-            self.get_logger().warn(f"Response time to reach {self._steering_command} mm: {response_time}")
-            self._steering_command *= -1
-            self._reached_goal = False
+        # if abs(self._steering_angle - self._steering_command) < 1e-3 and not self._reached_goal and self._send_time is not None:
+        #     self._reached_goal = True
+        #     response_time = self.get_time() - self._send_time
+        #     self.get_logger().warn(f"Response time to reach {self._steering_command} mm: {response_time}")
+        #     self._steering_command *= -1
+        #     self._reached_goal = False
         return
 
 
     def set_motor(self, msg:RxVehicleSensors) -> None:
         self._actual_torque = msg.motor_torque_actual
         # self.get_logger().info(f"Received motor actual torque {self._steering_angle}")
-        if abs(self._actual_torque - TORQUE_COMMAND) < 1e-3:
-            response_time = self.get_time() - self._send_time
-            self.get_logger().warn(f"Response time to reach {TORQUE_COMMAND} mm: {response_time}", once=True)
+        # if abs(self._actual_torque - TORQUE_COMMAND) < 1e-3:
+        #     response_time = self.get_time() - self._send_time
+        #     self.get_logger().warn(f"Response time to reach {TORQUE_COMMAND} mm: {response_time}", once=True)
         return
 
 
@@ -67,29 +68,31 @@ class InspectionMission(Node):
         
         msg = TxControlCommand()
 
-        if int(time) % 2 == 0:
-            msg.brake_pressure_target = True
-        else:
-            msg.brake_pressure_target = False
+        self._steering_command = self.get_parameter('brake').value
+        msg.brake_pressure_target = self._steering_command
+
+        self._steering_command = self.get_parameter('steering').value
+        msg.steering_angle_target = self._steering_command
+
+
         msg.speed_actual = 0
         msg.speed_target = 0
         msg.motor_torque_target = TORQUE_COMMAND
 
         # Normal inspection
         # msg.steering_angle_target = MAX_STEERING * sin(2*pi/STEERING_PERIOD * time)
-        msg.steering_angle_target = self._steering_command
+        # msg.steering_angle_target = self._steering_command
 
         # This is for dynamic setting of commands
         # prev = self._steering_command
-        # self._steering_command = self.get_parameter('steering').value
         # if self._steering_command != prev: 
-        #     self.get_logger().error("Why??")
-        #     self._send_time = None
-        #     self._reached_goal = False
-        # msg.steering_angle_target = self._steering_command
-        if self._send_time is None:
-            self.get_logger().warn(f'Steering command set to: {self._steering_command} mm')
-            self._send_time = self.get_time()
+            # self.get_logger().error("Why??")
+            # self._send_time = None
+            # self._reached_goal = False
+        
+        # if self._send_time is None:
+        #     self.get_logger().warn(f'Steering command set to: {self._steering_command} mm')
+        #     self._send_time = self.get_time()
         
         self._command_publisher.publish(msg)        
         
@@ -105,18 +108,18 @@ class InspectionMission(Node):
 
         msg.dv_status.id = self._status
 
-        msg.vn_200_ok = False
-        msg.vn_300_ok = False
-        msg.camera_left_ok = False
-        msg.camera_right_ok = False
+        msg.vn_200_error = False
+        msg.vn_300_error = False
+        msg.camera_left_error = False
+        msg.camera_right_error = False
 
-        msg.clock_ok = False
-        msg.camera_inference_ok = False
-        msg.velocity_estimation_ok = False
-        msg.slam_ok = False
-        msg.mpc_controls_ok = False
-        msg.path_planning_ok = False
-        msg.pi_pp_controls_ok = False
+        msg.clock_error = False
+        msg.camera_inference_error = False
+        msg.velocity_estimation_error = False
+        msg.slam_error = False
+        msg.mpc_controls_error = False
+        msg.path_planning_error = False
+        msg.pi_pp_controls_error = False
 
         msg.lap_counter = 0
         msg.cones_count_actual = 0
